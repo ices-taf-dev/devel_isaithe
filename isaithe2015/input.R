0e996ea93350d5964ed3422492efa9b5f409b918
Arni Magnusson <arnima@hafro.is>
2016-08-24 17:43:42 +0200

    Copy model.cfg and sam.pin from 'input' to 'run'

--- isaithe2015/input.R
+++ isaithe2015/input.R
@@ -1,5 +1,3 @@
 setwd("input")
 
-file.copy("model.cfg", "../run")
 file.copy("sam.dat",   "../run")
-file.copy("sam.pin",   "../run")
--- isaithe2015/model.R
+++ isaithe2015/model.R
@@ -3,4 +3,7 @@ setwd("run")
 sam <- if(.Platform$OS.type == "unix") "sam" else "sam.exe"
 
 file.copy(paste0("../model/",sam), ".")
+file.copy("../model/model.cfg", ".")
+file.copy("../model/sam.pin",   ".")
+
 system("sam -nr 2 -noinit")
